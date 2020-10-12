23a0dd416466231cffdf2203c3d9930419bae115
Maksim Makushchenko <maksalmak@gmail.com>
2020-10-12 13:48:24 +0300

    ivbo_07 threads

---
+++ src/ru/capchik/ivbo_07_19/lesson6/Threads.java
@@ -0,0 +1,43 @@
+package ru.capchik.ivbo_07_19.lesson6;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Threads {
+    //    static AtomicInteger totalSum = new AtomicInteger();
+    static int totalSum;
+    static ReentrantLock lock = new ReentrantLock();
+    public static void main(String[] args) throws InterruptedException {
+        long startTime = System.currentTimeMillis();
+        ArrayList<Thread> threads = new ArrayList<>();
+        for (int i = 0; i <  4; i++){
+            final int localI = i;
+            Thread thread = new Thread(() -> work(localI));
+            thread.start();
+            threads.add(thread);
+        }
+
+        for (Thread t : threads) {
+            t.join();
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("total time: " + (endTime - startTime));
+        System.out.println("total sum: " + totalSum);
+    }
+
+    private static void work(int i) {
+        long startTime = System.currentTimeMillis();
+        long result = doHardWork(i * 1000, 100_000_000);
+        long endTime = System.currentTimeMillis();
+        System.out.println(i + ": " + result + " | " + (endTime-startTime));
+    }
+
+    private synchronized static long doHardWork(int start, int count) {
+        long a = 0;
+        for (int i = 0; i < count; i++) {
+            a += (start + i) * (start + i) * Math.sqrt(start + i);
+            totalSum++;
+        }
+        return a;
+    }
+}
