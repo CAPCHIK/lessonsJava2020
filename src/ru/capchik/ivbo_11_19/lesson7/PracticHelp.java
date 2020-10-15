bc3229cb40d6a2ead080c80e0f568d1162995cac
Maksim Makushchenko <maksalmak@gmail.ccom>
2020-10-15 09:39:34 +0300

    ivbo 11 red fix

--- src/ru/capchik/ivbo_11_19/lesson7/PracticHelp.java
+++ src/ru/capchik/ivbo_11_19/lesson7/PracticHelp.java
@@ -2,7 +2,7 @@ package ru.capchik.ivbo_11_19.lesson7;
 
 public class PracticHelp {
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[34m";
+    public static final String ANSI_RED = "\u001B[31m";
 
     public static void main(String[] args) {
         System.out.println(ANSI_RED + "Red text example" + ANSI_RESET);
