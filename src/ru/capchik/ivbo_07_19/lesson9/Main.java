e9bb32663a2522d6c135b948f7208840db66a6f9
Maksim Makushchenko <maksalmak@gmail.com>
2020-11-02 13:42:12 +0300

    ivbo07 files

---
+++ src/ru/capchik/ivbo_07_19/lesson9/Main.java
@@ -0,0 +1,48 @@
+package ru.capchik.ivbo_07_19.lesson9;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Path path = Paths.get(".")
+                .toAbsolutePath()
+                .getParent()
+                .getParent();
+        String basePath = "src/ru/capchik/ivbo_07_19/lesson9";
+        String mainFile = basePath + "/Main.java";
+        String newFile = basePath + "/Copy.txt";
+        File file = new File(mainFile);
+        if (file.isDirectory()) {
+            String[] list = file.list();
+            for (String s : list) {
+                System.out.println(Paths.get(basePath, s).toAbsolutePath());
+            }
+        }
+        Scanner sc = new Scanner(System.in);
+        String mainFilePath = file.getAbsolutePath();
+        try {
+            try (
+                    BufferedReader reader = new BufferedReader(new FileReader(mainFilePath));
+                    PrintWriter writer = new PrintWriter(newFile)) {
+                String line = reader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    writer.write(line);
+                    writer.write('\n');
+                    sc.nextLine();
+                    line = reader.readLine();
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Incorrect file path");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(path);
+    }
+}
