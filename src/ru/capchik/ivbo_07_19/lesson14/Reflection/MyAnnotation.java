fd33e5d7e81571b4c8a3f36f53ff4ea4a8a65c02
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-12-17 20:49:38 +0300

    Update MyAnnotation.java

--- src/ru/capchik/ivbo_07_19/lesson14/Reflection/MyAnnotation.java
+++ src/ru/capchik/ivbo_07_19/lesson14/Reflection/MyAnnotation.java
@@ -1,1 +1,9 @@
+package ru.capchik.ivbo_07_19.lesson14.Reflection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface MyAnnotation {
+    String someField();
+}
