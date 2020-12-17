026b89e0df58d57aeaeb571427c0e6552fa149e5
Alexandra Oronovskaya <38656436+SunnyientDev@users.noreply.github.com>
2020-12-17 20:48:50 +0300

    Update ConsoleOperation.java

--- src/ru/capchik/ivbo_07_19/lesson14/Reflection/ConsoleOperation.java
+++ src/ru/capchik/ivbo_07_19/lesson14/Reflection/ConsoleOperation.java
@@ -1,1 +1,10 @@
+package ru.capchik.ivbo_07_19.lesson14.Reflection;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ConsoleOperation {
+    String title();
+    int key();
+}
